d7b492cd49dad76687c6999b23f783e9b3604780
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-07-09 20:42:13 -0500

    Secured API

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630003829) do
+ActiveRecord::Schema.define(version: 20150710013247) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150630003829) do
     t.string   "cuisine",     limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "api_key",    limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
